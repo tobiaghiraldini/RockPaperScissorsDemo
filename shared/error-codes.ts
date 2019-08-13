eba3ddb9356f66f963ecfe3667a80175e9bc7f88
Tobia Ghiraldini <tobia.ghiraldini@ninjabit.com>
2019-08-13 16:00:47 +0200

    Adding common request response handlers code

---
+++ shared/error-codes.ts
@@ -0,0 +1,11 @@
+// tslint:disable-next-line typedef variable-name (Acts like a type with static properties.)
+export const ErrorCode = {
+  GeneralError: 'GENERAL_ERROR',
+  InvalidBodyFormat: 'INVALID_BODY',
+  InvalidId: 'INVALID_ID',
+  InvalidMethod: 'INVALID_METHOD',
+  InvalidName: 'INVALID_NAME',
+  MissingEnv: 'MISSING_ENV',
+  MissingId: 'MISSING_ID',
+  MissingPermission: 'MISSING_PERMISSION'
+};

0d66b175a17cbd5c50b2138972d4c0164b09bfd1
Tobia Ghiraldini <tobia.ghiraldini@ninjabit.com>
2019-08-14 16:52:07 +0200

    Adding controller test case
    Still partly broken

--- src/game/game.controller.spec.ts
+++ src/game/game.controller.spec.ts
@@ -2,10 +2,10 @@ import { expect } from 'chai';
 import { Chance } from 'chance';
 import { instance, mock, reset, when } from 'ts-mockito';
 import { HttpStatusCode } from '../../shared/http-status-codes';
-import { callSuccess } from '../../test';
+import { callSuccess, postSuccess } from '../../test';
 import { ApiResponseParsed } from '../../test/test.interfaces';
 import { GameController } from './game.controller';
-import { GameMove, GameRequest, GameResult, ValidMoves, ValidResults } from './game.interfaces';
+import { GameItems, GameMove, GameRequest, GameResult, ValidMoves, ValidResults } from './game.interfaces';
 import { GameService } from './game.service';
 
 const chance: Chance.Chance = new Chance();
@@ -18,40 +18,44 @@ describe('GameController', () => {
   let resultData: GameResult;
   let gameMove1: GameMove;
   let gameMove2: GameMove;
-  requestData = {
-    move1: chance.pickone(ValidMoves),
-    move2: chance.pickone(ValidMoves)
-  };
-  resultData = {
-    move1: requestData.move1,
-    move2: requestData.move2,
-    result: chance.pickone(ValidResults)
-  };
 
   beforeEach(() => {
     reset(gameServiceMock);
     const gameServiceMockInstance: GameService = instance(gameServiceMock);
     controller = new GameController(gameServiceMockInstance);
+    requestData = {
+      move1: GameItems.paper,
+      move2: GameItems.rock
+    };
+    resultData = {
+      move1: requestData.move1,
+      move2: requestData.move2,
+      result: chance.pickone(ValidResults)
+    };
+    gameMove1 = {
+      choice: requestData.move1,
+    };
+    gameMove2 = {
+      choice: requestData.move2
+    };
   });
 
   describe('getResult function', () => {
-    it('Should return HTTP 200 OK', async () => {
-      gameMove1 = {
-        choice: requestData.move1,
-      };
-      gameMove2 = {
-        choice: requestData.move2
-      };
+    it('Should return HTTP 400 for GET', async () => {
+      when(gameServiceMock.isValid(gameMove1, gameMove2)).thenReturn(true);
       when(gameServiceMock.getResult(gameMove1, gameMove2))
-        .thenReturn(Promise.resolve<GameResult>(resultData));
+        .thenResolve((resultData));
       const response: ApiResponseParsed<GameResult> = await callSuccess<GameResult>(controller.getGameResult);
-      expect(response.statusCode).to.equal(HttpStatusCode.Ok);
+      expect(response.statusCode).to.equal(HttpStatusCode.BadRequest);
 
     });
     it('Should return the proper game result', async () => {
+      when(gameServiceMock.isValid(gameMove1, gameMove2)).thenReturn(true);
       when(gameServiceMock.getResult(gameMove1, gameMove2))
-        .thenReturn(Promise.resolve<GameResult>(resultData));
-      const response: ApiResponseParsed<GameResult> = await callSuccess<GameResult>(controller.getGameResult);
+        .thenResolve(resultData);
+      const response: ApiResponseParsed<GameResult> = await postSuccess<GameResult>(controller.getGameResult, JSON.stringify(requestData));
+      console.log("response")
+      console.log(response)
       expect(response.parsedBody.move1).to.oneOf(ValidMoves);
       expect(response.parsedBody.move2).to.oneOf(ValidMoves);
       expect(response.parsedBody.result).to.oneOf(ValidResults);

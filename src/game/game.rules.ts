0efb8a2b359c5ef3df7124cfcd27b664103b9952
Tobia Ghiraldini <tobia.ghiraldini@ninjabit.com>
2019-08-14 14:19:08 +0200

    Consolidating error codes

--- src/game/game.rules.ts
+++ src/game/game.rules.ts
@@ -65,7 +65,7 @@ export class RockPaperScissorsRules {
       default:
         return {
           ...baseResult,
-          result: 'Invalid game'
+          result: 'INVALID_CONTENT'
         };
     }
   }
